eaf81c8e32f802eac887718e43268d6fdaf1aede
lamlh@evolableasia.vn <lamlh@evolableasia.vn>
2018-12-03 15:23:34 +0700

    bill apis apply

--- cloud/modules/Bill.js
+++ cloud/modules/Bill.js
@@ -4,15 +4,15 @@ const UTILS = require('../../helpers/UTILS');
 
 const BillModule = {};
 
-BillModule.getBills = function(request) {
+BillModule.getBills = function (request) {
     const user = request.user;
 
     const billQuery = new Parse.Query('Bill');
     billQuery.equalTo('member', user);
     billQuery.descending('createdAt');
     billQuery.include('coupon');
-    billQuery.select(['objectId', 'barcode', 'discount', 'coupon.name', 'createdAt', 'total', 'totalProducts'])
-    
+    billQuery.select(['objectId', 'barcode', 'discount', 'coupon.name', 'createdAt', 'total', 'totalProducts']);
+
     const queryPaging = UTILS.pageCalc(request.params.page, request.params.perPage);
     if (queryPaging) {
         billQuery.skip(queryPaging.offset);
@@ -24,22 +24,25 @@ BillModule.getBills = function(request) {
     });
 };
 
-BillModule.getBillDetail = function(request) {
+BillModule.getBillDetail = function (request) {
+    let promises = [];
     const user = request.user;
+    const billObjectId = request.params.billObjectId;
 
     const billQuery = new Parse.Query('Bill');
-    billQuery.equalTo('member', user);
-    billQuery.descending('createdAt');
-    billQuery.include('coupon');
-    billQuery.select(['objectId', 'barcode', 'discount', 'coupon.name', 'createdAt', 'total', 'totalProducts'])
-    
-    const queryPaging = UTILS.pageCalc(request.params.page, request.params.perPage);
-    if (queryPaging) {
-        billQuery.skip(queryPaging.offset);
-        billQuery.limit(queryPaging.limit);
-    }
+    billQuery.equalTo('objectId', billObjectId);
+    billQuery.include(['coupon', 'employee']);
+    promises.push(billQuery.first());
 
-    return billQuery.find().then((result) => {
+    const billPointer = UTILS.createBlankPointerTo('Bill', billObjectId);
+    const billDetailQuery = new Parse.Query('BillDetail');
+    billDetailQuery.equalTo('bill', billPointer);
+    billDetailQuery.ascending('updatedAt');
+    billDetailQuery.include(['product', 'product.gift']);
+    billDetailQuery.select(['unitPrice', 'product.barcode', 'product.price', 'product.name', 'product.gift.name', 'isGiftIncluded', 'amount'])
+    promises.push(billDetailQuery.find());
+
+    return Promise.all(promises).then((result) => {
         return result;
     });
 };
