25746a95c71863da79008102723fdfaa128acf7f
lamlh@evolableasia.vn <lamlh@evolableasia.vn>
2018-12-03 16:42:56 +0700

    add totalproducts to get tags apis

--- cloud/modules/Tags.js
+++ cloud/modules/Tags.js
@@ -18,26 +18,38 @@ TagsModule.getTagsList = function (request) {
     return tagQuery.find().then((result) => {
         let promises = [];
 
-        if (includeSample) {
 
-            result.forEach((tag, index) => {
-                const sampleProductQuery = new Parse.Query('Product');
-                sampleProductQuery.ascending('objectId');
 
-                const innerQuery = new Parse.Query('Tag');
-                innerQuery.equalTo("objectId", tag.id);
+        result.forEach((tag, index) => {
+            let productPromises = [];
 
-                sampleProductQuery.matchesQuery("tag", innerQuery);
+            const countProducts = new Parse.Query('Product');
+            countProducts.ascending('objectId');
+
+            const innerQuery = new Parse.Query('Tag');
+            innerQuery.equalTo("objectId", tag.id);
+
+            countProducts.matchesQuery("tag", innerQuery);
+
+            productPromises.push(countProducts.count());
+
+            if (includeSample) {
+                const sampleProductQuery = _.clone(countProducts);
                 sampleProductQuery.limit(5);
+                productPromises.push(sampleProductQuery.find());
 
-                promises.push(sampleProductQuery.find().then((sampleList) => {
-                    tag = tag.toJSON();
-                    tag['sampleProduct'] = sampleList.map((product) => product.toJSON());
-                    result[index] = tag;
-                    return Promise.resolve(true);
-                }));
-            });
-        }
+            }
+
+            promises.push(Promise.all(productPromises).then((productResult) => {
+                const sampleList = includeSample ? productResult[1] : undefined;
+                const totalProducts = productResult[0];
+                tag = tag.toJSON();
+                tag['totalProducts'] = totalProducts;
+                tag['sampleProduct'] = sampleList ? sampleList.map((product) => product.toJSON()) : undefined;
+                result[index] = tag;
+                return Promise.resolve(true);
+            }));
+        });
 
         return Promise.all(promises).then(() => {
             return result;
